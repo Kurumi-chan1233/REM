71f4bede8d825df951b63855df841263692f4344
Kurumi-chan1233 <98651979+Kurumi-chan1233@users.noreply.github.com>
2022-02-07 19:29:43 +0700

    Add files via upload

--- plugins/viaovo.js
+++ plugins/viaovo.js
@@ -22,8 +22,8 @@ let handler = async (m, { conn }) => {
 ╠➥ *DAN 200 LEBIH*
 ║   *FITUR LAINNYA*
 ║ 
-╠═ ©2022 King Of Bear
-╠═ Scrip original by Nurutomo
+╠═ ©2022 Kurumi
+╠═ Scrip original by Kurumi-chan
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
 }
 
